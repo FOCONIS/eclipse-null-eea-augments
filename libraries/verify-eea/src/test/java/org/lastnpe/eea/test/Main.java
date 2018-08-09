a8089b77ce4d2ea087746f24ec30caff3ed3e6f1
Roland Praml <praml@foconis.de>
2018-08-09 15:31:51 +0200

    Fine tuning all-eea - separated verificator

--- libraries/verify-eea/src/test/java/org/lastnpe/eea/test/Main.java
+++ libraries/verify-eea/src/test/java/org/lastnpe/eea/test/Main.java
@@ -6,7 +6,7 @@ import java.time.OffsetDateTime;
 public class Main {
 
   // modify this line and run the class in eclipse (right click/run java application)
-  private static final Class<?> CLASS_TO_TEST = OffsetDateTime.class;
+  private static final Class<?> CLASS_TO_TEST = Double.class;
   
   public static void main(String[] args) throws IOException {
     EeaFile eea = EeaFile.parseClassFile("/" + CLASS_TO_TEST.getName().replace('.','/') + ".class");
